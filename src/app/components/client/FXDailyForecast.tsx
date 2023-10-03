c122cb97d01ac545362c0d6ca18e1ff1579d7ad7
Odin Falkenberg Sævrøy <os@imarex.no>
2023-10-03 14:58:30 +0200

    Highchart for currency pair USDNOK % EURNOK

--- src/app/components/client/FXDailyForecast.tsx
+++ src/app/components/client/FXDailyForecast.tsx
@@ -2,22 +2,84 @@
 // src/app/components/client/FXDailyForecast.tsx
 import React, { useEffect } from "react";
 
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+type ChartSeries = { name: string; data: { x: number; y: number }[] }[];
+
 const FXDailyForecast: React.FC = () => {
+  const [chartData, setChartData] = React.useState<ChartSeries | null>(null);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("/api/dailyForecast");
         const data = await response.json();
-        console.log(data);
+        processChartData(data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
+    const processChartData = (data: any) => {
+      const usdNokData = data.find((item: any) => item.pair === "USDNOK");
+      const eurNokData = data.find((item: any) => item.pair === "EURNOK");
+
+      const usdNokSeries = Object.keys(usdNokData)
+        .filter((key) => key.startsWith("date_"))
+        .map((key, index) => {
+          return {
+            x: new Date(usdNokData[key]).getTime(),
+            y: usdNokData[`price_${index}`],
+          };
+        });
+
+      const eurNokSeries = Object.keys(eurNokData)
+        .filter((key) => key.startsWith("date_"))
+        .map((key, index) => {
+          return {
+            x: new Date(eurNokData[key]).getTime(),
+            y: eurNokData[`price_${index}`],
+          };
+        });
+
+      setChartData([
+        { name: "USDNOK", data: usdNokSeries },
+        { name: "EURNOK", data: eurNokSeries },
+      ]);
+    };
+
     fetchData();
   }, []);
 
-  return <div>Check the console for data.</div>;
+  const options = {
+    title: {
+      text: "USDNOK / EURNOK",
+    },
+    xAxis: {
+      type: "datetime",
+    },
+    yAxis: {
+      title: {
+        text: "Price",
+      },
+    },
+    series: chartData,
+  };
+
+  return (
+    <div>
+      {chartData ? (
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+          containerProps={{ style: { width: "33%", height: "40%" } }}
+        />
+      ) : (
+        "Loading data..."
+      )}
+    </div>
+  );
 };
 
 export default FXDailyForecast;
