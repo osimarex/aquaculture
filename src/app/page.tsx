2178fc3e6c0a8e6f7f015bd408adb4db59687a50
Odin Falkenberg Sævrøy <os@imarex.no>
2023-10-03 10:17:44 +0200

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import List from "./components/client/List";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-blue-600">Aquaculture</h1>
+      <h1 className="text-red-600">Aquaculture</h1>
       <List />
     </main>
   );
