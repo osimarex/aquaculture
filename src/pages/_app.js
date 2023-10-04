14c0c49245814122fe92a8a1b434f0c1057b40fb
Odin Falkenberg Sævrøy <os@imarex.no>
2023-10-04 17:17:26 +0200

    Auth azure (not working)

---
+++ src/pages/_app.js
@@ -0,0 +1,15 @@
+import { MsalProvider } from "@azure/msal-react";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { msalConfig } from "../authConfig";
+
+export const msalInstance = new PublicClientApplication(msalConfig);
+
+export default function MainFunctionX({ Component, pageProps }) {
+  console.log("MainFunctionX()");
+
+  return (
+    <MsalProvider instance={msalInstance}>
+      <Component {...pageProps} />
+    </MsalProvider>
+  );
+}
