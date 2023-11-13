6047ae0bf36004ab43fec7f0b49ebc906f8103f7
Odin Falkenberg Sævrøy <os@imarex.no>
2023-11-13 10:32:56 +0100

    Update coordinates.ts

--- serverRendered/coordinates.ts
+++ serverRendered/coordinates.ts
@@ -6,7 +6,7 @@ export const areasGeoJson = {
       properties: {
         name: "Svenskegrensen til Jæren",
         biomasse: "10000 tonn",
-        romming: "3500",
+        romming: "3750",
         temperatur: "10 ℃",
       },
       geometry: {
