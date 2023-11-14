73c900fb9c69724355d641a268b7c38ec424765b
Odin Falkenberg Sævrøy <os@imarex.no>
2023-11-14 13:53:10 +0100

    Live prices on competing proteins

--- src/app/components/client/Proteins.tsx
+++ src/app/components/client/Proteins.tsx
@@ -1,42 +1,59 @@
-"use client";
-
 import React from "react";
+import useWebSocket from "@/app/hooks/useWebsocket";
 
 interface CompetingProteins {
   name: string;
   pricePerKg: number;
   date: string;
+  currency: "USD" | "GBP" | "AUD";
 }
 
 const proteins: CompetingProteins[] = [
-  { name: "Salmon", pricePerKg: 20.0, date: "01/01/23" },
-  { name: "Beef", pricePerKg: 15.0, date: "01/01/23" },
-  { name: "Pork", pricePerKg: 12.0, date: "01/01/23" },
-  { name: "Chicken", pricePerKg: 10.0, date: "01/01/23" },
-  { name: "Eggs", pricePerKg: 8.0, date: "01/01/23" },
-  { name: "Whey Protein", pricePerKg: 25.0, date: "01/01/23" },
-  { name: "Insect Protein", pricePerKg: 30.0, date: "01/01/23" },
+  { name: "Poultry", pricePerKg: 1.33, date: "10/11/23", currency: "USD" },
+  { name: "Pig", pricePerKg: 2.85, date: "10/11/23", currency: "USD" },
+  { name: "Bovine", pricePerKg: 8.93, date: "10/11/23", currency: "USD" },
+  { name: "Ovine", pricePerKg: 5.65, date: "10/11/23", currency: "AUD" },
+  { name: "White fish", pricePerKg: 19.66, date: "10/11/23", currency: "GBP" },
 ];
 
-const maxPrice = Math.max(...proteins.map((p) => p.pricePerKg));
-
-const getColor = (price: number, maxPrice: number): string => {
-  const percentage = price / maxPrice;
-  let hue;
-
-  if (percentage < 0.4) {
-    hue = 5 * percentage * 2; // Transition from red (0) to yellow (60) for the first half of the range
-  } else {
-    hue = 60 + 60 * (percentage - 0.5) * 2; // Transition from yellow (60) to green (120) for the second half of the range
-  }
-
-  const lightness = 30 + 30 * percentage; // Transition lightness from 50 to 75
-  const saturation = 85 + 25 * percentage; // Transition saturation from 75 to 100
-
-  return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-};
-
 const Protein: React.FC = () => {
+  const exchangeRates = useWebSocket();
+
+  const convertPriceToEur = (
+    price: number,
+    currency: "USD" | "GBP" | "AUD"
+  ): number => {
+    const usdNokRate = exchangeRates.USDNOK?.bid || 0;
+    const eurNokRate = exchangeRates.EURNOK?.bid || 0;
+    const conversionRateUsdToEur = usdNokRate / eurNokRate;
+    let priceInUsd;
+
+    switch (currency) {
+      case "USD":
+        priceInUsd = price;
+        break;
+      case "AUD":
+        const audToUsdRate = 0.72; // Assuming 1 AUD = 0.72 USD
+        priceInUsd = price * audToUsdRate;
+        break;
+      case "GBP":
+        const gbpToUsdRate = 1.31; // Assuming 1 GBP = 1.31 USD
+        priceInUsd = price * gbpToUsdRate;
+        break;
+      default:
+        priceInUsd = 0;
+    }
+    return priceInUsd * conversionRateUsdToEur;
+  };
+
+  const maxPrice = Math.max(
+    ...proteins.map((p) => convertPriceToEur(p.pricePerKg, p.currency))
+  );
+
+  // const getColor = (price: number, maxPrice: number): string => {
+  //   // ... Your existing getColor function ...
+  // };
+
   return (
     <div className="mt-4">
       <h2 className="ml-4 mb-4 font-semibold text-gray-800 dark:text-white">
@@ -54,13 +71,22 @@ const Protein: React.FC = () => {
           className="grid grid-cols-4 text-left border-t py-2 ml-4 mb-2 text-gray-800 dark:text-white"
         >
           <div>{protein.name}</div>
-          <div>${protein.pricePerKg.toFixed(2)}</div>
+          <div>
+            {`€${convertPriceToEur(
+              protein.pricePerKg,
+              protein.currency
+            ).toFixed(5)}`}
+          </div>
           <div>{protein.date}</div>
           <div className="relative h-10">
             <div
               className="absolute bottom-4 bg-green-400 sm:block hidden"
               style={{
-                width: `${(protein.pricePerKg / maxPrice) * 100}%`,
+                width: `${
+                  (convertPriceToEur(protein.pricePerKg, protein.currency) /
+                    maxPrice) *
+                  100
+                }%`,
                 height: "70%",
               }}
             ></div>
