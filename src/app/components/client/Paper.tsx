387be6532232d4b982ca93adac69f6569ce1fb19
Odin Falkenberg Sævrøy <os@imarex.no>
2023-11-21 16:31:09 +0100

    Update Paper.tsx

--- src/app/components/client/Paper.tsx
+++ src/app/components/client/Paper.tsx
@@ -4,7 +4,10 @@ import React, { useEffect, useState } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
-type ChartSeries = { name: string; data: { x: number; y: number }[] }[];
+type ChartSeries = {
+  name: string;
+  data: { x: number; y: number }[];
+}[];
 
 interface Props {
   darkMode: boolean;
@@ -12,11 +15,27 @@ interface Props {
 
 const SalmonForecast: React.FC<Props> = ({ darkMode }) => {
   const [chartData, setChartData] = useState<ChartSeries | null>(null);
+  const [weekNumbers, setWeekNumbers] = useState<string[]>([]);
+  const [prices, setPrices] = useState<{ [key: string]: number }>({});
+
+  useEffect(() => {
+    const fetchForwardPrices = async () => {
+      try {
+        const response = await fetch("/api/historicFuturePrices");
+        const priceData = await response.json();
+        const latestPriceData = priceData[priceData.length - 1];
+        setPrices(latestPriceData); // Store the latest prices
+      } catch (error) {
+        console.error("Error fetching forward prices:", error);
+      }
+    };
+    fetchForwardPrices();
+  }, []);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/dailyForecast");
+        const response = await fetch("/api/salmonForecast");
         const data = await response.json();
         processChartData(data);
       } catch (error) {
@@ -24,51 +43,83 @@ const SalmonForecast: React.FC<Props> = ({ darkMode }) => {
       }
     };
 
-    const processChartData = (data: any) => {
-      const usdNokData = data.find((item: any) => item.pair === "USDNOK");
-      const eurNokData = data.find((item: any) => item.pair === "EURNOK");
+    const processChartData = (data: any[]) => {
+      // Extract week numbers and remove duplicates
+      const weekNumbers: number[] = data.map((item) =>
+        parseInt(item.week.trim(), 10)
+      );
+      const uniqueWeekNumbers = Array.from(new Set(weekNumbers));
 
-      const usdNokSeries = Object.keys(usdNokData)
-        .filter((key) => key.startsWith("date_"))
-        .map((key, index) => {
-          return {
-            x: new Date(usdNokData[key]).getTime(),
-            y: usdNokData[`price_${index}`],
+      const seriesData = data.map((item) => {
+        const weekNumber = parseInt(item.week.trim(), 10);
+        const weekIndex = uniqueWeekNumbers.indexOf(weekNumber);
+        const price = parseFloat(item.forecasted_price.trim());
+        return { x: weekIndex, y: price };
+      });
+
+      setChartData([{ name: "Salmon Price", data: seriesData }]);
+      setWeekNumbers(uniqueWeekNumbers.map(String)); // Use this for the x-axis categories
+      addContractSeries(uniqueWeekNumbers);
+    };
+
+    const addContractSeries = (uniqueWeekNumbers: any[]) => {
+      // Calculate the current week number and the end week number for the 29-week range
+      const currentWeekNumber = getWeekNumber(new Date());
+
+      const endWeekNumber = currentWeekNumber + 29;
+
+      // Loop through each key in the prices object
+      Object.keys(prices).forEach((key) => {
+        const contractPrice = prices[key];
+
+        // Skip if the contract price is not a number or the key is '24'
+        if (isNaN(contractPrice) || key === "24") {
+          return;
+        }
+
+        // Calculate the week number for this contract
+        let contractWeekNumber = currentWeekNumber + parseInt(key, 10) * 4; // Assuming each 'key' represents roughly a month (4 weeks)
+        if (contractWeekNumber > 52) {
+          contractWeekNumber -= 52; // Adjust for year rollover
+        }
+
+        // Only add the contract if it falls within the 29-week range
+        if (
+          contractWeekNumber >= currentWeekNumber &&
+          contractWeekNumber <= endWeekNumber
+        ) {
+          const contractSeries = {
+            name: `Contract ${key}`,
+            data: uniqueWeekNumbers.map((_, index) => ({
+              x: index,
+              y: contractPrice,
+            })),
+            type: "line",
+            dashStyle: "Dash",
+            color: "#38B6FF",
           };
-        });
 
-      const eurNokSeries = Object.keys(eurNokData)
-        .filter((key) => key.startsWith("date_"))
-        .map((key, index) => {
-          return {
-            x: new Date(eurNokData[key]).getTime(),
-            y: eurNokData[`price_${index}`],
-          };
-        });
-
-      setChartData([
-        { name: "USDNOK", data: usdNokSeries },
-        { name: "EURNOK", data: eurNokSeries },
-      ]);
+          setChartData((prevChartData) =>
+            prevChartData
+              ? [...prevChartData, contractSeries]
+              : [contractSeries]
+          );
+        }
+      });
     };
 
     fetchData();
-  }, []);
+  }, [prices]);
 
-  useEffect(() => {
-    if (darkMode) {
-      Highcharts.theme = {
-        // ...dark theme properties
-      };
-    } else {
-      Highcharts.theme = {
-        // ...light theme properties
-      };
-    }
-    Highcharts.setOptions(Highcharts.theme);
-  }, [darkMode]);
+  // Helper function to calculate the week number of a date
+  const getWeekNumber = (date: Date) => {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear =
+      (date.getTime() - firstDayOfYear.getTime()) / 86400000;
+    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+  };
 
-  const optionsUSDNOK = {
+  const options = {
     chart: {
       height: 380,
       backgroundColor: darkMode ? "rgb(31 41 55)" : "#ffffff",
@@ -79,16 +130,8 @@ const SalmonForecast: React.FC<Props> = ({ darkMode }) => {
         color: darkMode ? "#ffffff" : "#000000",
       },
     },
-    credits: {
-      enabled: false,
-    },
-    plotOptions: {
-      series: {
-        lineWidth: 3,
-      },
-    },
     xAxis: {
-      type: "datetime",
+      categories: weekNumbers,
       labels: {
         style: {
           color: darkMode ? "#ffffff" : "#000000",
@@ -103,66 +146,203 @@ const SalmonForecast: React.FC<Props> = ({ darkMode }) => {
           color: darkMode ? "#ffffff" : "#000000",
         },
       },
-      range: 0.1,
-      labels: {
-        format: "{value:.2f}",
-        style: {
-          color: darkMode ? "#ffffff" : "#000000",
-        },
-      },
     },
-    legend: {
-      itemStyle: {
-        color: darkMode ? "#ffffff" : "#000000",
-      },
-    },
-    series: chartData
-      ?.filter((series) => series.name === "USDNOK")
-      .map((series) => ({
-        ...series,
-        type: "spline",
-        color: "#40ca16", // Default color is green
-        marker: {
-          enabled: false,
-        },
-        zones: [
-          {
-            value: series.data[0]?.y - 0.001, // Replace with your threshold value
-            color: "#fd2b2b", // Color will be red for values less than threshold
-          },
-          {
-            color: "#40ca16", // Color will revert to green for values greater or equal to threshold
-          },
-        ],
-      })),
+    series: chartData,
   };
 
   return (
     <div
-      className={`w-full h-auto ${
+      className={`w-full h-auto relative ${
         darkMode ? "text-white" : "bg-white text-black"
       }`}
     >
-      <div></div>
-      {chartData ? (
-        <div className="flex flex-col items-start mt-8 ml-1">
-          <div className="relative w-full">
-            <div className="absolute top-2 left-16 transform[-50%,-50%] z-10 text-2xl">
-              FORWARD PRICES
-            </div>
-            <div>
-              <HighchartsReact
-                highcharts={Highcharts}
-                options={optionsUSDNOK}
-              />
+      <div className="mt-14">
+        {chartData ? (
+          <div className="flex flex-col items-start mt-8 ml-1">
+            <div className="relative w-full">
+              <div className="absolute top-2 left-16 transform[-50%,-50%] z-10 text-2xl">
+                SALMON FORECAST
+              </div>
+              <div>
+                <HighchartsReact highcharts={Highcharts} options={options} />
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        "Loading data..."
-      )}
+        ) : (
+          "Loading data..."
+        )}
+      </div>
     </div>
   );
 };
 
 export default SalmonForecast;
+
+// "use client";
+
+// import React, { useEffect, useState } from "react";
+// import Highcharts from "highcharts";
+// import HighchartsReact from "highcharts-react-official";
+
+// type ChartSeries = { name: string; data: { x: number; y: number }[] }[];
+
+// interface Props {
+//   darkMode: boolean;
+// }
+
+// const SalmonForecast: React.FC<Props> = ({ darkMode }) => {
+//   const [chartData, setChartData] = useState<ChartSeries | null>(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch("/api/dailyForecast");
+//         const data = await response.json();
+//         processChartData(data);
+//       } catch (error) {
+//         console.error("Error fetching data:", error);
+//       }
+//     };
+
+//     const processChartData = (data: any) => {
+//       const usdNokData = data.find((item: any) => item.pair === "USDNOK");
+//       const eurNokData = data.find((item: any) => item.pair === "EURNOK");
+
+//       const usdNokSeries = Object.keys(usdNokData)
+//         .filter((key) => key.startsWith("date_"))
+//         .map((key, index) => {
+//           return {
+//             x: new Date(usdNokData[key]).getTime(),
+//             y: usdNokData[`price_${index}`],
+//           };
+//         });
+
+//       const eurNokSeries = Object.keys(eurNokData)
+//         .filter((key) => key.startsWith("date_"))
+//         .map((key, index) => {
+//           return {
+//             x: new Date(eurNokData[key]).getTime(),
+//             y: eurNokData[`price_${index}`],
+//           };
+//         });
+
+//       setChartData([
+//         { name: "USDNOK", data: usdNokSeries },
+//         { name: "EURNOK", data: eurNokSeries },
+//       ]);
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   useEffect(() => {
+//     if (darkMode) {
+//       Highcharts.theme = {
+//         // ...dark theme properties
+//       };
+//     } else {
+//       Highcharts.theme = {
+//         // ...light theme properties
+//       };
+//     }
+//     Highcharts.setOptions(Highcharts.theme);
+//   }, [darkMode]);
+
+//   const optionsUSDNOK = {
+//     chart: {
+//       height: 380,
+//       backgroundColor: darkMode ? "rgb(31 41 55)" : "#ffffff",
+//     },
+//     title: {
+//       text: "",
+//       style: {
+//         color: darkMode ? "#ffffff" : "#000000",
+//       },
+//     },
+//     credits: {
+//       enabled: false,
+//     },
+//     plotOptions: {
+//       series: {
+//         lineWidth: 3,
+//       },
+//     },
+//     xAxis: {
+//       type: "datetime",
+//       labels: {
+//         style: {
+//           color: darkMode ? "#ffffff" : "#000000",
+//         },
+//       },
+//     },
+//     yAxis: {
+//       gridLineColor: darkMode ? "#333333" : "#ededed",
+//       title: {
+//         text: "",
+//         style: {
+//           color: darkMode ? "#ffffff" : "#000000",
+//         },
+//       },
+//       range: 0.1,
+//       labels: {
+//         format: "{value:.2f}",
+//         style: {
+//           color: darkMode ? "#ffffff" : "#000000",
+//         },
+//       },
+//     },
+//     legend: {
+//       itemStyle: {
+//         color: darkMode ? "#ffffff" : "#000000",
+//       },
+//     },
+//     series: chartData
+//       ?.filter((series) => series.name === "USDNOK")
+//       .map((series) => ({
+//         ...series,
+//         type: "spline",
+//         color: "#40ca16", // Default color is green
+//         marker: {
+//           enabled: false,
+//         },
+//         zones: [
+//           {
+//             value: series.data[0]?.y - 0.001, // Replace with your threshold value
+//             color: "#fd2b2b", // Color will be red for values less than threshold
+//           },
+//           {
+//             color: "#40ca16", // Color will revert to green for values greater or equal to threshold
+//           },
+//         ],
+//       })),
+//   };
+
+//   return (
+//     <div
+//       className={`w-full h-auto ${
+//         darkMode ? "text-white" : "bg-white text-black"
+//       }`}
+//     >
+//       <div></div>
+//       {chartData ? (
+//         <div className="flex flex-col items-start mt-8 ml-1">
+//           <div className="relative w-full">
+//             <div className="absolute top-2 left-16 transform[-50%,-50%] z-10 text-2xl">
+//               FORWARD PRICES
+//             </div>
+//             <div>
+//               <HighchartsReact
+//                 highcharts={Highcharts}
+//                 options={optionsUSDNOK}
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       ) : (
+//         "Loading data..."
+//       )}
+//     </div>
+//   );
+// };
+
+// export default SalmonForecast;
