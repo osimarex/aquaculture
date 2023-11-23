371246f1aeae3ddc4c0c89e5b6177fdb1f737088
Odin Falkenberg Sævrøy <os@imarex.no>
2023-11-23 10:26:40 +0100

    added scrolling back, but dropdown removes it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,47 +23,47 @@ export default function Home() {
 
   return (
     <Theme>
-      <RemoveScroll as={Slot} allowPinchZoom enabled>
-        <main className={darkMode ? "dark" : ""}>
-          <div className="">
-            <Image
-              src={darkMode ? "/imarex-logo-dark.png" : "/imarex-logo.png"}
-              width={142}
-              height={52}
-              alt="Logo"
-              priority={true}
-            />
-            <hr className="border-cyan-500 border-2" />
+      {/* <RemoveScroll as={Slot} allowPinchZoom enabled> */}
+      <main className={darkMode ? "dark" : ""}>
+        <div className="">
+          <Image
+            src={darkMode ? "/imarex-logo-dark.png" : "/imarex-logo.png"}
+            width={142}
+            height={52}
+            alt="Logo"
+            priority={true}
+          />
+          <hr className="border-cyan-500 border-2" />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 cs:grid-cols-4 gap-4">
+          <div className="lg:ml-2 text-black dark:text-white h-fit w-full overflow-hidden rounded-lg bg-white shadow-xl dark:bg-gray-800 dark:border-transparent">
+            <List />
+            <FXDailyForecast darkMode={darkMode} />
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 cs:grid-cols-4 gap-4">
-            <div className="lg:ml-2 text-black dark:text-white h-fit w-full overflow-hidden rounded-lg bg-white shadow-xl dark:bg-gray-800 dark:border-transparent">
-              <List />
-              <FXDailyForecast darkMode={darkMode} />
-            </div>
-            <div className="lg:ml-2 text-black dark:text-white h-full w-full overflow-hidden border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:border-transparent">
-              <SalmonForecast darkMode={darkMode} />
-            </div>
-            <div className=" text-black dark:text-white h-full w-full overflow-hidden border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:border-transparent">
-              <Paper darkMode={darkMode} />
-            </div>
-            <div className=" h-full w-full overflow-scroll border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:border-transparent">
-              <Smolt darkMode={darkMode} />
-            </div>
-            <div className="lg:ml-2 h-full w-full overflow-hidden border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:text-white dark:border-transparent">
-              <Proteins />
-            </div>
-            <div className="cs:col-span-2 lg:ml-2 h-full w-full overflow-hidden border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:text-white dark:border-transparent">
-              <Biomass darkMode={darkMode} />
-            </div>
-            <div className=" lg:ml-2 text-white h-full w-full border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:border-transparent">
-              <ProductionAreas darkMode={darkMode} />
-            </div>
+          <div className="lg:ml-2 text-black dark:text-white h-full w-full overflow-hidden border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:border-transparent">
+            <SalmonForecast darkMode={darkMode} />
           </div>
-          <div className="text-black dark:text-white">
-            <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />{" "}
+          <div className=" text-black dark:text-white h-full w-full overflow-hidden border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:border-transparent">
+            <Paper darkMode={darkMode} />
           </div>
-        </main>
-      </RemoveScroll>
+          <div className=" h-full w-full overflow-scroll border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:border-transparent">
+            <Smolt darkMode={darkMode} />
+          </div>
+          <div className="lg:ml-2 h-full w-full overflow-hidden border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:text-white dark:border-transparent">
+            <Proteins />
+          </div>
+          <div className="cs:col-span-2 lg:ml-2 h-full w-full overflow-hidden border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:text-white dark:border-transparent">
+            <Biomass darkMode={darkMode} />
+          </div>
+          <div className=" lg:ml-2 text-white h-full w-full border-solid border-2 border-slate-200 rounded-xl bg-white shadow-xl dark:bg-gray-800 dark:border-transparent">
+            <ProductionAreas darkMode={darkMode} />
+          </div>
+        </div>
+        <div className="text-black dark:text-white">
+          <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />{" "}
+        </div>
+      </main>
+      {/* </RemoveScroll> */}
     </Theme>
   );
 }
