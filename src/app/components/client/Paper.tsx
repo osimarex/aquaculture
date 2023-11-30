c7072092f5d19fec7f721b52e400ecd4d1b893de
Odin Falkenberg Sævrøy <os@imarex.no>
2023-11-30 12:47:25 +0100

    Paper builds (color prop needed to be removed)

--- src/app/components/client/Paper.tsx
+++ src/app/components/client/Paper.tsx
@@ -4,73 +4,78 @@ import React, { useEffect, useState } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
-type ChartSeries = { name: string; data: { x: number; y: number }[] }[];
+type ChartPaperSeries = {
+  name: string;
+  data: { x: number; y: number }[];
+}[];
 
-interface Props {
+interface PropsPaper {
   darkMode: boolean;
 }
 
-const SalmonForecast: React.FC<Props> = ({ darkMode }) => {
-  const [chartData, setChartData] = useState<ChartSeries | null>(null);
+const PaperForecast: React.FC<PropsPaper> = ({ darkMode }) => {
+  const [chartPaperData, setChartPaperData] = useState<ChartPaperSeries | null>(
+    null
+  );
+  const [weekPaperNumbers, setWeekPaperNumbers] = useState<string[]>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchPaperData = async () => {
       try {
-        const response = await fetch("/api/dailyForecast");
+        const response = await fetch("/api/salmonForecast");
         const data = await response.json();
-        processChartData(data);
+        processChartPaperData(data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
-    const processChartData = (data: any) => {
-      const usdNokData = data.find((item: any) => item.pair === "USDNOK");
-      const eurNokData = data.find((item: any) => item.pair === "EURNOK");
+    const processChartPaperData = (data: any[]) => {
+      const weekPaperNumbers: number[] = data.map((item) =>
+        parseInt(item.week.trim(), 10)
+      );
+      const uniqueWeekPaperNumbers = Array.from(new Set(weekPaperNumbers));
 
-      const usdNokSeries = Object.keys(usdNokData)
-        .filter((key) => key.startsWith("date_"))
-        .map((key, index) => {
-          return {
-            x: new Date(usdNokData[key]).getTime(),
-            y: usdNokData[`price_${index}`],
-          };
-        });
+      const seriesData = data.map((item) => {
+        const weekPaperNumber = parseInt(item.week.trim(), 10);
+        const weekIndex = uniqueWeekPaperNumbers.indexOf(weekPaperNumber);
+        const price = parseFloat(item.forecasted_price.trim());
+        return { x: weekIndex, y: price };
+      });
 
-      const eurNokSeries = Object.keys(eurNokData)
-        .filter((key) => key.startsWith("date_"))
-        .map((key, index) => {
-          return {
-            x: new Date(eurNokData[key]).getTime(),
-            y: eurNokData[`price_${index}`],
-          };
-        });
+      const additionalSeries = [
+        { startWeek: 48, endWeek: 1, price: 85.5, name: "1st Month" },
+        { startWeek: 1, endWeek: 5, price: 106.1, name: "2nd Month" },
+        { startWeek: 1, endWeek: 14, price: 109.1, name: "Q1" },
+        { startWeek: 14, endWeek: 22, price: 112.3, name: "Q2" },
+      ].map((item) => {
+        const startIndex = uniqueWeekPaperNumbers.indexOf(item.startWeek);
+        const endIndex = uniqueWeekPaperNumbers.indexOf(item.endWeek);
+        return {
+          name: item.name,
+          data: [
+            { x: startIndex, y: item.price },
+            { x: endIndex, y: item.price },
+          ],
+        };
+      });
 
-      setChartData([
-        { name: "USDNOK", data: usdNokSeries },
-        { name: "EURNOK", data: eurNokSeries },
+      setChartPaperData([
+        { name: "Salmon Price", data: seriesData },
+        ...additionalSeries.map((series, index) => ({
+          ...series,
+          color: "#40ca16",
+        })),
       ]);
+      setWeekPaperNumbers(uniqueWeekPaperNumbers.map(String));
     };
 
-    fetchData();
+    fetchPaperData();
   }, []);
 
-  useEffect(() => {
-    if (darkMode) {
-      Highcharts.theme = {
-        // ...dark theme properties
-      };
-    } else {
-      Highcharts.theme = {
-        // ...light theme properties
-      };
-    }
-    Highcharts.setOptions(Highcharts.theme);
-  }, [darkMode]);
-
-  const optionsUSDNOK = {
+  const options = {
     chart: {
-      height: 255,
+      height: 380,
       backgroundColor: darkMode ? "rgb(31 41 55)" : "#ffffff",
     },
     title: {
@@ -79,87 +84,70 @@ const SalmonForecast: React.FC<Props> = ({ darkMode }) => {
         color: darkMode ? "#ffffff" : "#000000",
       },
     },
-    credits: {
-      enabled: false,
+    xAxis: {
+      categories: weekPaperNumbers,
+      labels: {
+        style: {
+          color: darkMode ? "#ffffff" : "#000000",
+        },
+      },
     },
     plotOptions: {
       series: {
         lineWidth: 3,
       },
     },
-    xAxis: {
-      type: "datetime",
-      labels: {
-        style: {
-          color: darkMode ? "#ffffff" : "#000000",
-        },
+    legend: {
+      itemStyle: {
+        color: darkMode ? "#ffffff" : "#000000",
       },
     },
     yAxis: {
-      gridLineColor: darkMode ? "" : "",
+      gridLineColor: darkMode ? "#333333" : "#ededed",
       title: {
         text: "",
         style: {
           color: darkMode ? "#ffffff" : "#000000",
         },
       },
-      range: 0.1,
-      labels: {
-        format: "{value:.2f}",
-        style: {
-          color: darkMode ? "#ffffff" : "#000000",
-        },
-      },
     },
-    legend: {
+
+    credits: {
       enabled: false,
     },
-    series: chartData
-      ?.filter((series) => series.name === "USDNOK")
-      .map((series) => ({
-        ...series,
-        type: "spline",
-        color: "#40ca16", // Default color is green
-        marker: {
-          enabled: false,
-        },
-        zones: [
-          {
-            value: series.data[0]?.y - 0.001, // Replace with your threshold value
-            color: "#fd2b2b", // Color will be red for values less than threshold
-          },
-          {
-            color: "#40ca16", // Color will revert to green for values greater or equal to threshold
-          },
-        ],
-      })),
+    series: chartPaperData?.map((series) => ({
+      ...series,
+      type: "spline",
+      marker: {
+        enabled: false,
+      },
+    })),
   };
 
   return (
     <div
-      className={`w-full h-auto ${
+      className={`w-full h-auto relative ${
         darkMode ? "text-white" : "bg-white text-black"
       }`}
     >
-      {chartData ? (
-        <div className="flex flex-col items-start mt-4 ml-1">
-          <div className="relative w-full">
-            <div className="absolute top-2 left-16 transform[-50%,-50%] z-10 text-2xl">
-              FORWARD PRICES
-            </div>
-            <div>
-              <HighchartsReact
-                highcharts={Highcharts}
-                options={optionsUSDNOK}
-              />
+      <div className="mt-14">
+        {chartPaperData ? (
+          <div className="flex flex-col items-start mt-8 ml-1">
+            <div className="relative w-full">
+              <div className="absolute top-2 left-16 transform[-50%,-50%] z-10 text-2xl">
+                FORWARD PRICE
+              </div>
+              <div>
+                <HighchartsReact highcharts={Highcharts} options={options} />
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        "Loading data..."
-      )}
+        ) : (
+          "Loading data..."
+        )}
+      </div>
     </div>
   );
 };
 
-export default SalmonForecast;
+export default PaperForecast;
